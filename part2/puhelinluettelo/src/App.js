e3b3e72f5504fb2b9805e80a30589283925d7fcb
laasov <lassi.o.virtanen@gmail.com>
2022-05-28 16:03:49 +0300

    puhelinluettelo OK

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,15 +1,45 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios'
+import idService from './services/phonebook'
 
-const PhoneId = ({ person, number }) => <p>{person} {number}</p>
+/*
 
-const NameList = ({ persons, filt }) => {
+Completed all tasks up to 2.18
+Tested -- all seem to work OK
+
+*/
+
+
+// Called from NameList
+const PhoneId = person => <p>{person.person.name} {person.person.number} <DeleteButton person={person.person}/></p>
+
+// Called from DeleteButton
+const removeId = person => {
+  if (window.confirm(`Delete ${person.person.name} ?`)) {
+    idService.remove(person.person.id)
+    window.location.reload()
+  }
+}
+
+// Called from PhoneId
+const DeleteButton = person => {
   return (
-  persons.flatMap(person =>
-    person.name.toLowerCase().includes(filt.toLocaleLowerCase()) ? <PhoneId key={person.name} person={person.name} number={person.number}/> : '')
+    <button onClick={() => removeId(person)}>
+      delete
+    </button>
   )
 }
 
+// Called from App independently
+const NameList = ({ persons, filt }) => {
+  const flc = filt.toLowerCase()
+
+  return (
+    persons.flatMap(person => 
+      person.name.toLowerCase().includes(flc) ? <PhoneId key={person.name} person={person}/> : '')
+  )
+}
+
+// Called from App idependently
 const FilterForm = ({filter, handle}) => {
   return (
     <form>
@@ -22,6 +52,7 @@ const FilterForm = ({filter, handle}) => {
   )
 }
 
+// Called from App independently
 const IdList = (props) => {
   return (
     <form onSubmit={props.concatName}>
@@ -54,37 +85,61 @@ const App = () => {
 
   useEffect(() => {
     console.log('effect')
-    axios.get('http://localhost:3001/persons').then(respose => {
-      console.log('promise fulfilled')
-      setPersons(respose.data)
+    idService
+      .getAll()
+      .then(respose => {
+        console.log('promise fulfilled')
+        setPersons(respose.data)
     })
   }, [])
   console.log('render', persons.length, 'persons')
+
 
   const handleNamePrompt = (e) => {
     console.log('handleNamePrompt event:', e.target.value)
     setNewName(e.target.value)
   }
 
+
   const handleNumPrompt = (e) => {
     console.log('handleNumPrompt event:', e.target.value)
     setNewNum(e.target.value)
   }
 
+
   const handleFilter = (e) => {
     setFilt(e.target.value)
   }
 
+
+  const handleClick = () => {
+    console.log("Clicked")
+  }
+
   const concatName = (e) => {
     e.preventDefault()
-    console.log('concatName event target value:', e.target.value)
-
+    
     if (persons.map(person => person.name).includes(newName)) {
-      alert(`${newName} is already added to phonebook`)
+      const pers = persons.find(pers => pers.name === newName)
+
+      idService
+        .update(pers.name, newNum, pers.id)
+        .then(response => {
+          window.confirm(`${newName} is already added to phonebook, replace old number with a new one?`)
+          window.location.reload()
+        })
     } else {
-      setPersons(persons.concat( {name: newName, number: newNum} ))
+      idService
+        .create( {name: newName, number: newNum} )
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName('')
+          setNewNum('')
+        })
+      window.location.reload()
     }
   }
+  
 
   return (
     <div>
@@ -99,12 +154,11 @@ const App = () => {
               handleNumPrompt={handleNumPrompt} />
       <h2>Numbers</h2>
       <NameList persons={persons}
-                filt={filt} />
+                filt={filt} 
+                handleClick={handleClick}/>
     </div>
   )
 
 }
-//{persons.flatMap(person => person.name.toLowerCase().includes(filt.toLocaleLowerCase()) ? <p>{person.name} {person.number}</p> : '')}
-// <PhoneId key={person.name} person={person.name} number={person.number}/> : <PhoneId key={person.name} person={'asd'} number={'asd'}
 
 export default App
---
+++ part2/puhelinluettelo/src/services/phonebook.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/persons'
+
+
+const getAll = () => {
+    return axios.get(baseUrl)
+}
+
+const create = newId => {
+    return axios.post(baseUrl, newId)
+}
+
+const update = (nam, num, id) => {
+    return axios.put(`${baseUrl}/${id}`, {name : nam, number : num})
+}
+
+const remove = id => {
+    return axios.delete(`${baseUrl}/${id}`)
+}
+
+
+export default { getAll, create, update, remove }
